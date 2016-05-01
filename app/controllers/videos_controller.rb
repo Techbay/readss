e55d705c03d291a17d79f9ff22308131a9ecaa73
caok <caok1231@gmail.com>
2016-05-01 09:10:22 +0800

    Refs #17 - View Design For Video Index Page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
   before_action :set_video, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
 
   # GET /videos
   # GET /videos.json
