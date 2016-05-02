5be14517d8eb1c6928f35e30ec61aa327aa7181c
Yu Liu <yul.liuyu@gmail.com>
2016-05-02 23:30:33 +0000

    #6 reward addition/subtraction and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  def add_reward(change=0)
+    self.reward += change
+  end
+  
+  def subtract_reward(change=0)
+    self.reward = [0, self.reward - change].max
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,32 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "user_creation" do
+    new_user = User.new()
+    assert(new_user.reward == 0, failure_message="default reward is not zero")
+  end
+  
+  test "user_update_reward" do
+    new_user = User.new()
+    
+    #add points
+    change = 10
+    result = new_user.reward + 10
+    new_user.add_reward(change)
+    assert(new_user.reward == result, failure_message = "reward addition not correct")
+    
+    #subtract points
+    change = 5
+    result = new_user.reward - 5
+    new_user.subtract_reward(change)
+    assert(new_user.reward == result, failure_message = "reward subtraction not correct")
+    
+    #reward points cannot be negative
+    change = new_user.reward + 3
+    new_user.subtract_reward(change)
+    assert(new_user.reward >= 0 , failure_message = "reward cannot be negative")
+  end
+  
+  
 end
