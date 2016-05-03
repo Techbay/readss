e0ded8195c22f3c1bf6c85fc69e36f5f9957c6d3
xiongbo <xiongbo027@gmail.com>
2016-05-03 20:41:59 +0800

    refs #2 update video fetching

--- app/models/video.rb
+++ app/models/video.rb
@@ -15,24 +15,40 @@ class Video < ApplicationRecord
     items = Youtube::Base.playlist_items
     items.each do |i|
       video = i.video
-      find_or_create_by(video_type: "Youtube",
-                        title: video.title,
-                        summary: video.description,
-                        video_id: video.id,
-                        published_at: video.published_at,
-                        thumbnail_url: video.thumbnail_url,
-                        channel_id: video.channel_id,
-                        channel_title: video.channel_title,
-                        category_id: video.category_id,
-                        category_title: video.category_title,
-                        view_count: video.view_count,
-                        like_count: video.like_count,
-                        dislike_count: video.dislike_count,
-                        favorite_count: video.favorite_count,
-                        comment_count: video.comment_count,
-                        duration: video.duration,
-                        is_hd: video.hd?,
-                        embed_html: video.embed_html)
+      begin
+        entry = find_or_create_by(video_id: video.id)
+        entry.update(
+          title: video.title,
+          summary: video.description,
+          video_type: "Youtube",
+          published_at: video.published_at,
+          thumbnail_url: video.thumbnail_url,
+          channel_id: video.channel_id,
+          channel_title: video.channel_title,
+          category_id: video.category_id,
+          category_title: video.category_title,
+          view_count: video.view_count,
+          like_count: video.like_count,
+          dislike_count: video.dislike_count,
+          favorite_count: video.favorite_count,
+          comment_count: video.comment_count,
+          duration: video.duration,
+          is_hd: video.hd?,
+          embed_html: video.embed_html)
+      rescue
+        next
+      end
+    end
+  end
+
+  private
+  def self.fetch_item(items, num)
+    index = 0
+    items.each do |i|
+      if index == num
+        return i
+      end
+      index += 1
     end
   end
 end
