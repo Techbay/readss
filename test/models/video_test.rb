6087b7e63bf71558641d7f5f214f6610127e181c
Yu Liu <yul.liuyu@gmail.com>
2016-05-07 02:53:58 +0000

    #12 revert video_test.rb to be consistent with new md_to_html! method

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -29,7 +29,7 @@ class VideoTest < ActiveSupport::TestCase
       end
     end
   end
-
+  
   module InstanceMethodTest
     def test_md_to_html
       input = <<-INPUT
@@ -48,15 +48,14 @@ look like:
 Note that --- not considering the asterisk --- the actual text
 content starts at 4-columns in.
       INPUT
-
+      
       output = <<-OUTPUT
-      <h1>An h1 header</h1>\n\n<p>Paragraphs are separated by a blank line.</p>\n\n<p>2nd paragraph. <em>Italic</em>, <strong>bold</strong>, and <code>monospace</code>. Itemized lists\nlook like:</p>\n\n<ul>\n<li>this one</li>\n<li>that one</li>\n<li>the other one</li>\n</ul>\n\n<p>Note that --- not considering the asterisk --- the actual text\ncontent starts at 4-columns in.</p>\n
-      OUTPUT
-
+<h1>An h1 header</h1>\n\n<p>Paragraphs are separated by a blank line.</p>\n\n<p>2nd paragraph. <em>Italic</em>, <strong>bold</strong>, and <code>monospace</code>. Itemized lists\nlook like:</p>\n\n<ul>\n<li>this one</li>\n<li>that one</li>\n<li>the other one</li>\n</ul>\n\n<p>Note that --- not considering the asterisk --- the actual text\ncontent starts at 4-columns in.</p>
+OUTPUT
       assert_equal(output, videos(:test_md2html).md_to_html(input))
     end
   end
-
+  
   def test_md_to_html!
     # test no field
     # test wrong field
@@ -67,14 +66,16 @@ content starts at 4-columns in.
       videos(:test_md2html).summary = "* abc"
       videos(:test_md2html).md_to_html!("summary")
     end
-
+    
     # test conversion
+    videos(:test_md2html).summary_md = "* abc"
+    videos(:test_md2html).md_to_html!(:summary_md, :summary) 
+    assert_equal("<ul>\n<li>abc</li>\n</ul>\n", videos(:test_md2html).summary)
+    
     # confirm that markdown conversion not performed if summary_md not changed
     # not done with this part
     videos(:test_md2html).update(:title => "a new title")
-    videos(:test_md2html).summary = "* abc"
-    videos(:test_md2html).md_to_html!(:summary)
-    assert_equal("<ul>\n<li>abc</li>\n</ul>\n", videos(:test_md2html).summary)
+    
   end
 
   include ClassMethodTest
