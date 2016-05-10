39378bb835c1359d7fbf906490516df9158e0104
xiongbo <xiongbo027@gmail.com>
2016-05-10 10:05:40 +0800

    remove youtube key from initializer

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -2,7 +2,4 @@ Yt.configure do |config|
   config.client_id = ENV["G_CLIENT_ID"]
   config.client_secret = ENV["G_SECRET"]
   config.api_key = ENV["G_API_KEY"]
-  # config.client_id = "1008827501354-s6hc38qbvoebutcjbjetsbtp4ef8g97a.apps.googleusercontent.com"
-  # config.client_secret = "dKiRDH5eSgwmThojNxcZBtyo"
-  # config.api_key = "AIzaSyBkTDiiydQFN0qElB4_m_2hJ-ORI2y-Pqo"
 end
