8c1c9d7a94cd0890ff21b390b2c884fad19549dd
xiongbo <xiongbo027@gmail.com>
2016-05-12 16:21:42 +0800

    refs #24 update reward api

--- app/controllers/apis/v1/users_controller.rb
+++ app/controllers/apis/v1/users_controller.rb
@@ -1,19 +1,21 @@
 module Apis::V1
   class UsersController < ApplicationController
+    protect_from_forgery with: :null_session
 
     def add_reward
       ApiRequest.create(address: request.fullpath, from: request.env["HTTP_REFERER"])
       User.first.add_reward(user_rewards)
+      render text: "success"
     end
 
     def subtract_reward
       User.first.subtract_reward(user_rewards)
+      render text: "success"
     end
 
     private
     def user_rewards
       params[:reward]
     end
-
   end
 end
