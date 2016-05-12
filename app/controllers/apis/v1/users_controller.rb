37bd59a77c97c518b6b7249b16527c12f81d57cc
xiongbo <xiongbo027@gmail.com>
2016-05-12 11:26:55 +0800

    add api request model

---
+++ app/controllers/apis/v1/users_controller.rb
@@ -0,0 +1,18 @@
+module Apis::V1
+  class UsersController < ApplicationController
+
+    def add_reward
+      User.first.add_reward(user_rewards)
+    end
+
+    def subtract_reward
+      User.first.subtract_reward(user_rewards)
+    end
+
+    private
+    def user_rewards
+      params.require(:users).permit(:reward)
+    end
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   def add_reward(change=0)
     self.reward += change
+    save!
   end
-  
+
   def subtract_reward(change=0)
     self.reward = [0, self.reward - change].max
+    save!
   end
 end
