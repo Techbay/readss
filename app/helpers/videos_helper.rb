abdc91f48125c9325873ff0d7f694de93243baa5
Yu Liu <yul.liuyu@gmail.com>
2016-05-15 21:38:52 +0000

    #26 refactor: use videos_helper methods to generate views.

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,22 @@
 module VideosHelper
+    # show if the video is redeemed or not
+    def video_redeem_status(video)
+      if user_signed_in?
+        if current_user.videos.include?(video)
+            content_tag :span, "You have purchased this video!", {style: "color: blue"}
+        else
+            link_to "兑换", "/videos/"+video.id.to_s,  class: "redeem-video", 
+          data: {video_id: video.id, confirm: "Click OK to use your reward to watch " + video.title.upcase}
+        end
+      end
+    end
+    
+    # generate the video thumbnail or embedded video 
+    def video_content(video)
+      if user_signed_in? and current_user.videos.include?(video) 
+        raw video.embed_html
+      else
+        image_tag(video.thumbnail_url||"administrate/search.svg", alt: "READSS") 
+      end
+    end 
 end
