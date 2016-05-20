21592b18a05efa4b3980d280357e0c317ad6909e
Yu Liu <yul.liuyu@gmail.com>
2016-05-20 05:47:51 +0000

    #29 add devise_i18n gem and setup locale routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  namespace :admin do
-    resources :users
-    resources :lists
-    resources :videos
-    resources :api_requests
-
-    root to: "users#index"
-  end
-
-  namespace :apis do
-    namespace :v1 do
-      get 'add_reward', to: 'users#add_reward'
+  scope "/:locale", locale: /en|zh-CN/ do
+    devise_for :users
+  
+    namespace :admin do
+      resources :users
+      resources :lists
+      resources :videos
+      resources :api_requests
+      
+      get '/:locale' => 'users#index' 
+      root to: "users#index"
     end
-  end
-
-  resources :videos
-  post 'video_redeem', to: 'videos#redeem'
+  
+    namespace :apis do
+      namespace :v1 do
+        get 'add_reward', to: 'users#add_reward'
+      end
+    end
+  
+    resources :videos
+    post 'video_redeem', to: 'videos#redeem'
+    get 'user_id', to: 'application#user_id'
+    
+  end 
+  get '/:locale' => 'videos#index' 
   root to: 'videos#index'
-  get 'user_id', to: 'application#user_id'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
