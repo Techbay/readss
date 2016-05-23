e304376f57f3e57f159f75554986ca45f5fe5ff4
xiongbo <xiongbo027@gmail.com>
2016-05-23 16:59:14 +0800

    add locale changing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_locale
-  
 
   def user_id
     if current_user
@@ -12,13 +11,12 @@ class ApplicationController < ActionController::Base
       render plain: "no_user"
     end
   end
-  
+
   def default_url_options(options = {})
-  { locale: I18n.locale }.merge options
-  end 
-  
+    { locale: I18n.locale }.merge options
+  end
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
   scope "(/:locale)", locale: /en|zh-CN/ do
     devise_for :users
-  
+
     namespace :admin do
       resources :users
       resources :lists
       resources :videos
       resources :api_requests
-      
-      get '/:locale' => 'users#index' 
+
+      get '/:locale' => 'users#index'
       root to: "users#index"
     end
-  
+
     namespace :apis do
       namespace :v1 do
         get 'add_reward', to: 'users#add_reward'
       end
     end
-  
+
     resources :videos
     post 'video_redeem', to: 'videos#redeem'
     get 'user_id', to: 'application#user_id'
-    
-  end 
-  get '/:locale' => 'videos#index' 
+
+  end
+
+  get '/:locale' => 'videos#index'
   root to: 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
