a89cbafd08033963c44f8c81d20a29d930ad54ef
Yu Liu <yul.liuyu@gmail.com>
2016-05-25 19:15:38 +0000

    #32 add tags to view. no mark up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create(email: "test@techbay.com", password: "12345678", reward: 10, isadmin: true)
 List.create(name: "programming", rid: "PLxsn9ryMcqFRaF8rXgwkwb6SMzBNWhCri", source_type: "youtube")
 List.create(name: "test", rid: "27270192", source_type: "youku")
-
+Tag.create(name: "rails")
+Tag.create(name: "html")
 # Suppose we use youtube
 List.all.each do |l|
   l.fetching_videos
