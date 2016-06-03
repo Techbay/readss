8c690c9b83b3b8e0dadb5451e18f4b22dc6f4058
Yu Liu <yul.liuyu@gmail.com>
2016-06-03 20:20:04 +0000

    #30 log_in feature

--- features/log_in.feature
+++ features/log_in.feature
@@ -1,5 +1,9 @@
 Feature: Log In
 Scenario: Successful log in
-  When I am on the homepage
-  And Click "Log In"
-  Then I should see a form
+  When User "test@techbay.com":"12345678" has logged in
+  Then I should see "rewards"
+  And I should see "test@techbay.com"
+Scenario: Unsuccessful log in
+  When User "test@techbay.com":"12345678" has not logged in
+  Then I should not see "rewards"
+  And I should not see "test@techbay.com"
--- features/step_definitions/log_in_steps.rb
+++ features/step_definitions/log_in_steps.rb
@@ -1,9 +1,24 @@
-When(/^Click "([^"]*)"$/) do |link|
-  # Write code here that turns the phrase above into concrete actions
-  click_link(link)
+When(/^User "([^"]*)":"([^"]*)" has( not)? logged in$/) do |email, password, bool|
+  to_sign_in = !bool
+  if to_sign_in
+    sign_in_user(email, password)# Write code here that turns the phrase above into concrete actions
+  else
+    sign_in_user(email, password*2)
+  end
 end
 
-Then(/^I should see a form$/) do
-  # Write code here that turns the phrase above into concrete actions
-  assert page.has_content?("All Videos")
+Then(/^I should( not)? see "([^"]*)"$/) do |bool, str|
+  expected = !bool
+  actual = page.has_content?(str)
+  msg = 'I should#{bool} see #{str}#'
+  assert_equal expected, actual, msg
+end
+
+
+def sign_in_user(email, password)
+  visit root_path
+  click_link("Log In")
+  fill_in("inputEmail", with: email)
+  fill_in("inputPassword", with: password)
+  click_button("Sign in")
 end
